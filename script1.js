053323b65bbaf182d06b4b124e932b8bf1f976c5
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2023-05-15 22:32:49 -0700

    add player animations

--- script1.js
+++ script1.js
@@ -1,6 +1,11 @@
 /**
  * @type {HTMLCanvasElement}
  */
+let playerState = "idle";
+const dropdown = document.getElementById("animations");
+dropdown.addEventListener("change", function(e){
+  playerState = e.target.value;
+})
 
 const canvas  = document.getElementById("canvas2");
 const ctx = canvas.getContext("2d");
@@ -11,18 +16,40 @@ const playerImage = new Image();
 playerImage.src = "shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
+
 let gameFrame = 0;
-const interval = 5;
+const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+  {name: "idle", frames: 7},
+  {name: "jump", frames: 7},
+  {name: "fall", frames: 7},
+  {name: "run", frames: 9},
+  {name: "dizzy", frames: 11},
+  {name: "sit", frames: 5},
+  {name: "roll", frames: 7},
+  {name: "bite", frames: 7},
+  {name: "ko", frames: 12},
+  {name: "gethit", frames: 4},
+]
+
+animationStates.forEach((state, index) => {
+  let frames = { loc: []}
+  for(let j=0; j<state.frames; j++){
+    let positionX = j*spriteWidth;
+    let positionY = index*spriteHeight;
+    frames.loc.push({x: positionX, y: positionY});
+  }
+  spriteAnimations[state.name] = frames;
+})
 
 function animate() {
   ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-  ctx.drawImage(playerImage, frameX*spriteWidth, frameY*spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-  if(gameFrame % interval == 0 ) {
-    if(frameX < 6) frameX++;
-    else frameX = 0;
-  }
+  let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
+  let frameX = position * spriteWidth;
+  let frameY = spriteAnimations[playerState].loc[position].y;
+  ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
   gameFrame++;
   requestAnimationFrame(animate);
 }
